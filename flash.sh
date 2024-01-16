9a6079101cf3e3e6a8b82ef4ecc5e28f6ab64958
RetroBitLab <icjtqr@gmail.com>
2024-01-16 18:41:34 +0100

    The flash.sh command now flash only the application in hex format.

--- flash.sh
+++ flash.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-EXEC=$1
-if [ $# -ne 1 ]; then
-	EXEC=build/stm32f401-usb-keyboard-amiga.elf
+EXEC=build/stm32f401-usb-keyboard-amiga.hex
+if [ ! -f ${EXEC} ]; then
+	echo "Build Application first. Please run 'make clean' & 'make' !"
+	exit 1
 fi
 openocd -f openocd/stm32f4eval.cfg \
 -c "init; targets; reset init; wait_halt; poll; flash write_image erase unlock ${EXEC}; reset run; shutdown"
