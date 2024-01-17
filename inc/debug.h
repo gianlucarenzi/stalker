46dca873f0a428acb9a90436fe8e8270357adc17
RetroBitLab <icjtqr@gmail.com>
2024-01-17 10:53:57 +0100

    Added more ANSI Colors

--- inc/debug.h
+++ inc/debug.h
@@ -11,11 +11,24 @@ extern "C" {
 #include <stdio.h>
 
 /* ANSI Eye-Candy ;-) */
-#define ANSI_RED    "\x1b[31m"
-#define ANSI_GREEN  "\x1b[32m"
-#define ANSI_YELLOW "\x1b[1;33m"
-#define ANSI_BLUE   "\x1b[1;34m"
-#define ANSI_RESET  "\x1b[0m"
+#define ANSI_RESET   "\x1b[0m"
+#define ANSI_BLACK   "\x1b[30m"
+#define ANSI_RED     "\x1b[31m"
+#define ANSI_GREEN   "\x1b[32m"
+#define ANSI_YELLOW  "\x1b[33m"
+#define ANSI_BLUE    "\x1b[34m"
+#define ANSI_MAGENTA "\x1b[35m"
+#define ANSI_CYAN    "\x1b[36m"
+#define ANSI_WHITE   "\x1b[37m"
+
+#define ANSI_LIGHT_BLACK   "\x1b[1;30m"
+#define ANSI_LIGHT_RED     "\x1b[1;31m"
+#define ANSI_LIGHT_GREEN   "\x1b[1;32m"
+#define ANSI_LIGHT_YELLOW  "\x1b[1;33m"
+#define ANSI_LIGHT_BLUE    "\x1b[1;34m"
+#define ANSI_LIGHT_MAGENTA "\x1b[1;35m"
+#define ANSI_LIGHT_CYAN    "\x1b[1;36m"
+#define ANSI_LIGHT_WHITE   "\x1b[1;37m"
 
 #define printR(fmt, args...) \
 	{\
