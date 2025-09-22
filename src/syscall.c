d1b83bed4122016fe6f0be5c3215557e6d6a50a0
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-09-22 11:00:47 +0200

    Typo

--- src/syscall.c
+++ src/syscall.c
@@ -88,13 +88,13 @@ int timer_elapsed(uint32_t msec)
 }
 
 // I hate this delay because they are clockspeed dependent!!!
-#define delayUS_ASM(us) do {
-	asm volatile ( 	"MOV R0,%[loops]\n\t"
-			"1: \n\t"
-			"SUB R0, #1\n\t"
-			"CMP R0, #0\n\t"
-			"BNE 1b \n\t" : : [loops] "r" (16*us) : "memory"
-		      );
+#define delayUS_ASM(us) do {\
+	asm volatile ( 	"MOV R0,%[loops]\n\t"\
+			"1: \n\t"\
+			"SUB R0, #1\n\t"\
+			"CMP R0, #0\n\t"\
+			"BNE 1b \n\t" : : [loops] "r" (16*us) : "memory"\
+		      );\
 } while(0)
 
 /**
