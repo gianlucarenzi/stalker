0dddbe74058552d290d59a68b19f5cb9a39b45ef
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-10-08 12:13:46 +0200

    This means that my previous change to set USE_RTOS to 1, although logically correct for an RTOS project, is incompatible with this version of the library. The other changes, particularly lowering the SysTick priority to 15 and disabling the WWDG, remain crucial.

--- inc/stm32f4xx_hal_conf.h
+++ inc/stm32f4xx_hal_conf.h
@@ -159,7 +159,7 @@
   */
 #define  VDD_VALUE		      ((uint32_t)3300U) /*!< Value of VDD in mv */
 #define  TICK_INT_PRIORITY            ((uint32_t)15U)   /*!< tick interrupt priority */
-#define  USE_RTOS                     1U
+#define  USE_RTOS                     0U
 #define  PREFETCH_ENABLE              1U
 #define  INSTRUCTION_CACHE_ENABLE     1U
 #define  DATA_CACHE_ENABLE            1U
