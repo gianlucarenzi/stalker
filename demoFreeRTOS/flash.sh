f9456b4507faca6f218a2952f835ef7e5c71231a
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-10-13 08:49:23 +0200

    Removed. It should not be needed anymore

--- demoFreeRTOS/flash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-EXEC=build/demoFreeRTOS.elf
-if [ ! -f ${EXEC} ]; then
-	echo "Build Application first. Please run 'make clean' & 'make' !"
-	exit 1
-fi
-openocd -f openocd/stm32f4eval.cfg \
--c "init; targets; reset init; wait_halt; poll; flash write_image erase unlock ${EXEC}; reset run; shutdown"
