798821a9474d4b3777c5baedeaf6491b8b72ca1a
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-11-06 13:39:59 +0100

    feat: Implementata emulazione EEPROM robusta con scrittura flash bufferizzata
    Questo commit risolve un problema per cui il microcontrollore si poteva bloccare durante le operazioni di scrittura della EEPROM, in particolare all'inizializzazione di una EEPROM non valida. La precedente implementazione tentava di cancellare un settore flash che probabilmente conteneva il codice in esecuzione, causando un crash/reset del sistema.
    
    Le modifiche principali includono:
    o Script del Linker (`stm32f401rctx_flash.ld`)
      Aggiornata la dimensione della flash a 256KB (STM32F401RC).
      Dedicato il FLASH_SECTOR_1 (16KB) all'emulazione EEPROM, isolandolo dal codice dell'applicazione.
    
    o Header EEPROM (`inc/eeprom.h`):
      Aggiornata EEPROM_SIZE a 16KB.
      Aggiunto l'attributo `__attribute__((section(".RamFunc")))` alla dichiarazione di `eeprom_write` per l'esecuzione da RAM.
      Corretta la dichiarazione di `_eeprom_start` a `uint32_t[]` per risolvere un avviso di `memcpy` sui limiti dell'array.
    
    o Sorgente EEPROM (`src/eeprom.c`):
      Implementata una strategia di scrittura bufferizzata: copia il settore flash in RAM, modifica in RAM, cancella il settore flash e poi riscrive l'intero buffer RAM nella flash. Questo garantisce l'integrità dei dati e la resilienza in caso di perdita di alimentazione.
      Assicurato che `eeprom_write` venga eseguita dalla RAM per prevenire conflitti di esecuzione durante le operazioni flash.
    
    Queste modifiche forniscono un'emulazione EEPROM più sicura e affidabile, prevenendo crash e garantendo un corretto avvio del sistema anche con EEPROM non inizializzata.

--- inc/eeprom.h
+++ inc/eeprom.h
@@ -4,15 +4,15 @@
 #include "stm32f4xx_hal.h"
 
 // Define the size of the EEPROM emulation
-#define EEPROM_SIZE 4096
+#define EEPROM_SIZE 0x4000
 
 // Define the start address of the EEPROM emulation
 // This address must be defined in the linker script
-extern uint32_t _eeprom_start;
+extern uint32_t _eeprom_start[];
 
 // Function prototypes
 HAL_StatusTypeDef eeprom_read(uint32_t address, uint32_t *data);
-HAL_StatusTypeDef eeprom_write(uint32_t address, uint32_t data);
+__attribute__((section(".RamFunc"))) HAL_StatusTypeDef eeprom_write(uint32_t address, uint32_t data);
 
 #define EEPROM_MODE_CONFIG 0x00
 
