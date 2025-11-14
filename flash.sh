c905ef4c5462d9394f238bbc861a042dea574a12
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-11-14 17:01:36 +0100

    Updated

--- flash.sh
+++ flash.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-EXEC=build/stm32f401-usb-keyboard-amiga.hex
+EXEC=$1
+
+if [ "${EXEC}" == "" ]; then
+	EXEC=build/stm32f401-usb-keyboard-amiga.hex
+fi
 if [ ! -f ${EXEC} ]; then
 	echo "Build Application first. Please run 'make clean' & 'make' !"
 	exit 1
 fi
 openocd -f openocd/stm32f4eval.cfg \
--c "init; targets; reset init; wait_halt; poll; flash write_image erase unlock ${EXEC}; reset run; shutdown"
+-c "init; targets; reset init; wait_halt; poll; program ${EXEC} verify reset; shutdown"
