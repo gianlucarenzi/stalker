eff3bd6bd73042504839c118df3cfa856da3bbbb
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-11-14 16:41:13 +0100

    Updated

--- stalkb-bootloader/Core/Inc/main.h
+++ stalkb-bootloader/Core/Inc/main.h
@@ -7,7 +7,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2022 STMicroelectronics.
+  * Copyright (c) 2025 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -28,6 +28,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
+#include <stdio.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -57,7 +58,52 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define BOOT_MODE_Pin GPIO_PIN_1
+#define BOOT_MODE_GPIO_Port GPIOC
+#define LED_PIN_Pin GPIO_PIN_0
+#define LED_PIN_GPIO_Port GPIOA
+#define UART_TX_Pin GPIO_PIN_2
+#define UART_TX_GPIO_Port GPIOA
+#define UART_RX_Pin GPIO_PIN_3
+#define UART_RX_GPIO_Port GPIOA
+
 /* USER CODE BEGIN Private defines */
+// ANSI Color Codes
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+// Debug Levels
+#define DEBUG_LEVEL_NONE    0 // Only errors
+#define DEBUG_LEVEL_INFO    1 // Errors + Info
+#define DEBUG_LEVEL_DEBUG   2 // Errors + Debug
+#define DEBUG_LEVEL_ALL     3 // Errors + Info + Debug
+
+// Configure current debug level here
+#define CURRENT_DEBUG_LEVEL DEBUG_LEVEL_ALL
+
+// Software Version
+#define SOFTWARE_VERSION "1.0"
+
+// Custom Print Macros
+#define PRINT_NORMAL(fmt, ...)  printf(ANSI_COLOR_RESET fmt ANSI_COLOR_RESET, ##__VA_ARGS__)
+#define PRINT_ERROR(fmt, ...)   printf(ANSI_COLOR_RED "ERROR: " fmt ANSI_COLOR_RESET, ##__VA_ARGS__)
+
+#if CURRENT_DEBUG_LEVEL >= DEBUG_LEVEL_INFO
+#define PRINT_INFO(fmt, ...)    printf(ANSI_COLOR_GREEN "INFO: " fmt ANSI_COLOR_RESET, ##__VA_ARGS__)
+#else
+#define PRINT_INFO(fmt, ...)
+#endif
+
+#if CURRENT_DEBUG_LEVEL >= DEBUG_LEVEL_DEBUG
+#define PRINT_DEBUG(fmt, ...)   printf(ANSI_COLOR_YELLOW "DEBUG: " fmt ANSI_COLOR_RESET, ##__VA_ARGS__)
+#else
+#define PRINT_DEBUG(fmt, ...)
+#endif
 
 /* USER CODE END Private defines */
 
--- stalkb-bootloader/Core/Inc/stm32f4xx_it.h
+++ stalkb-bootloader/Core/Inc/stm32f4xx_it.h
@@ -6,14 +6,14 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2022 STMicroelectronics.
+  * Copyright (c) 2025 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
- ******************************************************************************
+  ******************************************************************************
   */
 /* USER CODE END Header */
 
@@ -22,7 +22,7 @@
 #define __STM32F4xx_IT_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Private includes ----------------------------------------------------------*/
@@ -55,16 +55,7 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
-void OTG_FS_IRQHandler(void);
 /* USER CODE BEGIN EFP */
-
-/* the following symbols are defined in the GNU linker script */
-extern uint32_t *__bootflash_start;
-extern uint32_t *__bootflash_end;
-extern uint32_t *__appflash_start;
-extern uint32_t *__appflash_end;
-extern uint32_t *__ram_start;
-extern uint32_t *__ram_end;
 
 /* USER CODE END EFP */
 
